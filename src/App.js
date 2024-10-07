0d5d995a4e51a434092084952edd14eebf296806
Manuk-Han <aksdnr507@gmail.com>
2024-10-07 18:43:39 +0900

    OAuth2 login google

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LoginPage from "./user/member/LoginPage";
-import SignupPage from "./user/member/SignupPage";
 
 const App = () => {
   return (
       <Router>
         <Routes>
           <Route path="/" element={<LoginPage />} />
-          <Route path="/signup" element={<SignupPage />} />
+          {/*<Route path="/signup" element={<SignupPage />} />*/}
         </Routes>
       </Router>
   );
--- src/user/member/LoginPage.jsx
+++ src/user/member/LoginPage.jsx
@@ -1,62 +1,22 @@
 import React, {useState} from 'react';
-import { useNavigate } from 'react-router-dom';
 import './css/LoginPage.css';
 
 const LoginPage = () => {
-    const [loginInfo, setLoginInfo] = useState({ username: '', password: '' });
-
-    const navigate = useNavigate();
-
-    const handleSignupClick = () => {
-        navigate('/signup');
-    };
-
-    const handleInputChange = (e) => {
-        const { name, value } = e.target;
-        setLoginInfo({ ...loginInfo, [name]: value });
-    };
-
-    const handleLogin = (e) => {
-        e.preventDefault();
-        //TODO: 로그인 처리
-        console.log('로그인 정보:', loginInfo);
+    const handleOAuthLogin = (provider) => {
+        window.location.href = `http://localhost:8080/oauth2/authorization/${provider}`;
     };
 
     return (
         <div className="login-page">
             <div className="login-container">
                 <h2>로그인</h2>
-                <form onSubmit={handleLogin}>
-                    <div className="input-group">
-                        <label htmlFor="username">아이디</label>
-                        <input
-                            type="text"
-                            id="username"
-                            name="username"
-                            value={loginInfo.username}
-                            onChange={handleInputChange}
-                            required
-                        />
-                    </div>
-                    <div className="input-group">
-                        <label htmlFor="password">비밀번호</label>
-                        <input
-                            type="password"
-                            id="password"
-                            name="password"
-                            value={loginInfo.password}
-                            onChange={handleInputChange}
-                            required
-                        />
-                    </div>
-                    <button type="submit" className="login-button">로그인</button>
-                </form>
-                <div className="extra-options">
-                    <button className="link-button" onClick={() => handleSignupClick()}>
-                        회원가입
+                <div className="oauth-options">
+                    <h3>OAuth2</h3>
+                    <button className="oauth-button kakao" onClick={() => handleOAuthLogin('kakao')}>
+                        카카오로 회원가입
                     </button>
-                    <button className="link-button" onClick={() => alert('ID/PW 찾기 페이지로 이동')}>
-                        아이디/비밀번호 찾기
+                    <button className="oauth-button google" onClick={() => handleOAuthLogin('google')}>
+                        구글로 회원가입
                     </button>
                 </div>
             </div>
