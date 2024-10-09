280cc68904856562a56ce75143be514a14fe5e30
Manuk-Han <aksdnr507@gmail.com>
2024-10-09 17:38:48 +0900

    jsx -> tsx

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import LoginPage from "./user/member/LoginPage";
+import LoginPage from "./user/member/LoginPage.tsx";
+import HomePage from "./home/HomePage";
 
 const App = () => {
   return (
@@ -8,6 +9,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<LoginPage />} />
           {/*<Route path="/signup" element={<SignupPage />} />*/}
+            <Route path="/home" element={<HomePage />} />
         </Routes>
       </Router>
   );
