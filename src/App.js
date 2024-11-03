de927cba9fc7165c57f592b9a4403253fdb006f2
Manuk-Han <aksdnr507@gmail.com>
2024-11-03 20:42:30 +0900

    add kakaopay redirect

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import LoginPage from './user/member/LoginPage';
 import HomePage from './home/HomePage';
-import SignupPage from './user/member/SignupPage';
 import HomeDetailPage from "./home/HomeDetailPage";
-import {PageTabs} from "./redux/Tabs";
+import { PageTabs } from "./redux/Tabs";
 import Payment from "./content/payment/Payment";
 import OAuth2RedirectHandler from "./user/member/OAuth2RedirectHandler";
+import PaymentSuccess from "./content/payment/PaymentSuccess";
+import PaymentCancel from "./content/payment/PaymentCancel";
+import PaymentFail from "./content/payment/PaymentFail";
 
 const App = () => {
     return (
@@ -19,6 +21,11 @@ const App = () => {
                 <Route path="/oauth2/redirect" element={<OAuth2RedirectHandler />} />
                 <Route path="/product/:id" element={<HomeDetailPage />} />
                 <Route path="/payment/:productId" element={<Payment />} />
+
+                <Route path="/order/pay/completed" element={<PaymentSuccess />} />
+                <Route path="/order/pay/cancel" element={<PaymentCancel />} />
+                <Route path="/order/pay/fail" element={<PaymentFail />} />
+
                 <Route path="*" element={<Navigate to="/" />} />
             </Routes>
         </Router>
---
+++ src/content/payment/PaymentCancel.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const PaymentCancel = () => {
+    return (
+        <div>
+            <h1>결제 취소</h1>
+            <p>결제가 취소되었습니다. 다시 시도해 주세요.</p>
+        </div>
+    );
+};
+
+export default PaymentCancel;
