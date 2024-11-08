2838716a1f9a443262af0479663dd765b6b64302
Manuk-Han <aksdnr507@gmail.com>
2024-11-08 15:51:21 +0900

    add kakaopay in cart page

--- src/content/payment/PaymentSuccess.tsx
+++ src/content/payment/PaymentSuccess.tsx
@@ -1,8 +1,7 @@
 import React, { useEffect } from 'react';
-import axios from 'axios';
-import {useSelector} from "react-redux";
-import {RootState} from "../../redux/store";
-import {useNavigate} from "react-router-dom";
+import { useSelector } from "react-redux";
+import { RootState } from "../../redux/store";
+import { useNavigate } from "react-router-dom";
 import axiosInstance from "../../config/axiosInstance";
 
 const PaymentSuccess = () => {
@@ -13,39 +12,44 @@ const PaymentSuccess = () => {
         const recordOrder = async () => {
             const tid = localStorage.getItem("tid");
             const pgToken = new URLSearchParams(window.location.search).get("pg_token");
-            const productId = localStorage.getItem("productId");
-            const quantity = localStorage.getItem("quantity");
+            const purchaseId = new URLSearchParams(window.location.search).get("purchaseId");
 
-            if (!tid || !pgToken || !productId || !quantity) {
-                console.error("결제 정보가 부족합니다.");
+            if(!tid && !pgToken && !purchaseId) {
                 return;
+            } else if(!tid || !pgToken || !purchaseId) {
+                console.log('tid:', tid);
+                console.log('pgToken:', pgToken);
+                console.log('purchaseId:', purchaseId);
+
+                navigate('/payment/fail');
             }
 
-            const formData = new FormData();
-            formData.append("tid", tid);
-            formData.append("pgToken", pgToken);
-            formData.append("productId", productId);
-            formData.append("quantity", quantity);
+            if(tid && pgToken && purchaseId) {
+                const approveForm = new FormData();
+                approveForm.append("tid", tid);
+                approveForm.append("pgToken", pgToken);
+                approveForm.append("purchaseId", purchaseId);
 
-            try {
-                await axiosInstance.post('/payment/success', formData, {
-                    headers: {
-                        Authorization: `${accessToken}`,
-                        "Content-Type": "application/x-www-form-urlencoded"
+                try {
+                    await axiosInstance.post('/payment/success', approveForm,
+                        {
+                            headers: {
+                                'Content-Type': 'application/x-www-form-urlencoded',
+                            }
+                        });
+
+                    localStorage.removeItem("tid");
+
+                    const url = new URL(window.location.href);
+                    url.searchParams.delete("purchaseId");
+                    url.searchParams.delete("pg_token");
+                    window.history.replaceState({}, document.title, url.toString());
+                } catch (error) {
+                    console.error("주문 기록 중 에러 발생:", error);
+                    if (error.response?.status === 401) {
+                        alert("세션이 만료되었습니다. 다시 로그인 해 주세요.");
+                        navigate('/sign-in');
                     }
-                });
-
-                localStorage.removeItem("tid");
-                localStorage.removeItem("productId");
-                localStorage.removeItem("quantity");
-
-                const url = new URL(window.location.href);
-                url.searchParams.delete("pg_token");
-            } catch (error) {
-                console.error("주문 기록 중 에러 발생:", error);
-                if (error.response?.status === 401) {
-                    alert("세션이 만료되었습니다. 다시 로그인 해 주세요.");
-                    navigate('/sign-in');
                 }
             }
         };
